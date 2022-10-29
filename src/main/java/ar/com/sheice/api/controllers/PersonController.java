56a18b180cafb273d98a41674440729354602089
Sheice <75450007+Sheice@users.noreply.github.com>
2022-10-29 13:05:17 -0300

    controller get all person done

--- src/main/java/ar/com/sheice/api/controllers/PersonController.java
+++ src/main/java/ar/com/sheice/api/controllers/PersonController.java
@@ -1,10 +1,25 @@
 package ar.com.sheice.api.controllers;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.sheice.api.models.Person;
+import ar.com.sheice.api.services.PersonService;
 
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
     
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping("/all")
+    public ArrayList<Person> getAllPerson(){
+        return personService.getAllPersons();
+    }
+
 }
