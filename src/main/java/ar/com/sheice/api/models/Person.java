b751c38b3f1891404bda30ffa66e9bdb809270d7
Sheice <75450007+Sheice@users.noreply.github.com>
2022-10-29 12:49:39 -0300

    model person done

---
+++ src/main/java/ar/com/sheice/api/models/Person.java
@@ -0,0 +1,26 @@
+package ar.com.sheice.api.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String biography;
+
+}
