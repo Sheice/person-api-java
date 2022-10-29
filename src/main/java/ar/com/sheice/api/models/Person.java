ded70b25a8a1e55e64f894e362d825d118177018
Sheice <75450007+Sheice@users.noreply.github.com>
2022-10-29 12:52:11 -0300

    generating setter an getter and constructors by person

--- src/main/java/ar/com/sheice/api/models/Person.java
+++ src/main/java/ar/com/sheice/api/models/Person.java
@@ -21,6 +21,64 @@ public class Person {
     @Column(name = "last_name")
     private String lastName;
 
+    @Column(length = 500)
     private String biography;
+
+    
+
+    public Person(Long id, String firstName, String lastName, String biography) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.biography = biography;
+    }
+
+    public Person() {
+    }
+
+    public Person(Long id) {
+        this.id = id;
+    }
+
+    public Person(String firstName, String lastName, String biography) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.biography = biography;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    
+
 
 }
